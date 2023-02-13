ee79923ef5922ce735709bd2ee928ca63941aff2
albim72 <marcin.albiniak72@gmail.com>
2023-02-13 15:07:42 +0100

    Update Pojazd.py

--- DZIEN_1/POJAZD_KOSZTY/Pojazd.py
+++ DZIEN_1/POJAZD_KOSZTY/Pojazd.py
@@ -1,8 +1,17 @@
 from IPojazd import IPojazd
+from ISilnik import ISilnik
 
-class Pojazd(IPojazd):
+class Pojazd(IPojazd,ISilnik):
     def spalanie(self, odl, litry):
         return litry*100/odl
 
     def kosztprzejazdu(self, odl, litry, cena_l):
         return self.spalanie(odl,litry)*(odl/100)*cena_l
+
+    def dane_silnika(self, rodzaj, poj):
+        return f"rodzaj: {rodzaj}, pojemność: {poj}"
+
+    def stan(self, opis, ilekm, remonty):
+        return f"stan silnika -> remonty: {remonty}, liczba przejechanych km: {ilekm}, " \
+               f"informacje dodatkowe: {opis}"
+    
