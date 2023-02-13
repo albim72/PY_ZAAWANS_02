8ba62e505f3952946104d0916ddd026b5818fa0c
albim72 <marcin.albiniak72@gmail.com>
2023-02-13 11:07:26 +0100

    Update programowanie_funkcyjne.py

--- DZIEN_1/programowanie_funkcyjne.py
+++ DZIEN_1/programowanie_funkcyjne.py
@@ -1,3 +1,6 @@
+import time
+import math
+
 #przykład 1
 def startstop(funkcja):
     def wrapper(*args):
@@ -31,3 +34,54 @@ def osoba(funkcja,*args):
 
 print(osoba(nowyuczestnik,"Leon"))
 print(osoba(wystapienie,"Anna",7))
+
+#przykład 3
+
+def pomiarczasu(funkcja):
+    def wrapper():
+        starttime = time.perf_counter()
+        funkcja()
+        endtime = time.perf_counter()
+        print(f'czas wykonania funkcji {funkcja.__name__}: {endtime-starttime} s')
+    return wrapper
+
+def sleep(funkcja):
+    def wrapper():
+        time.sleep(3)
+        return funkcja()
+    return wrapper
+
+@sleep
+@pomiarczasu
+def biglista():
+    sum([i**3 for i in range(1000000)])
+
+biglista()
+
+@sleep
+def info():
+    return "opóźniona funkcja"
+
+print(info())
+
+
+#przykład 4
+def repeater(n):
+    def wrapper(funkcja):
+        def inner(*args):
+            for i in range(n):
+                funkcja(*args)
+        return inner
+    return wrapper
+
+@repeater(n=6)
+def komunikat(k,n):
+    print(f'ważby komunikat nr {n}: {k}')
+
+komunikat("fsdfdjfs5555555",78)
+
+@repeater(11)
+def hx(c):
+    print((c-1)**6)
+
+hx(3)
