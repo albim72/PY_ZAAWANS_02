4dca74ac287a2dd5ae2008373f0a0dc4b7a60733
albim72 <marcin.albiniak72@gmail.com>
2023-02-13 11:32:15 +0100

    Update statystyki.py

--- DZIEN_1/statystyki.py
+++ DZIEN_1/statystyki.py
@@ -1,1 +1,9 @@
-liczby = [56,245,-66,0,12,1987,-999,112,87,234,-87,555,-222,0,456]
+liczby = [56,245,-66,0,12,1987,-999,112,87,234,-87,555,-222,0,456,12]
+liczbykrt = (99,3345,2,5,-23,3,3)
+liczbyzb = {45,67,-8,9,0,2,3,4,2,3}
+
+def pokaz_statystyki(kolekcja):
+    minimum = min(kolekcja)
+    maksimum = max(kolekcja)
+    rozstep = maksimum - minimum
+    l_elem = len(kolekcja)
