4df7aa9daf99e8ece93f0cdb9c91195fca5725e2
albim72 <marcin.albiniak72@gmail.com>
2023-02-13 12:27:26 +0100

    Create cdane.py

---
+++ DZIEN_1/MOJE_MODULY/KLASY/cdane.py
@@ -0,0 +1,12 @@
+from funkcje.bfunkcje import *
+
+class CDane:
+    def __init__(self,lista,slownik):
+        self.lista = lista
+        self.slownik = slownik
+        
+    def czytajlst(self):
+        return czytaj_liste(self.lista)
+    
+    def czytajslow(self):
+        return czytaj_slownik(self.slownik)
