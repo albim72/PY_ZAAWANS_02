5b4800a9a0f7d0d9ad37257f85646574a0b0cb1a
albim72 <marcin.albiniak72@gmail.com>
2023-02-14 14:04:29 +0100

    Update main.py

--- DZIEN_2/FIRMA/main.py
+++ DZIEN_2/FIRMA/main.py
@@ -14,6 +14,7 @@ boss = Boss()
 s = AkwizytorNaEtacie('Jan','Kowalski','444564',Decimal('756000'),Decimal('4.50'),Decimal('3500'))
 c = Akwizytor('Olga','Kot','976565',Decimal('756000'),Decimal('5.3'))
 
+
 wyplaty = [c,s,boss]
 
 for wp in wyplaty:
