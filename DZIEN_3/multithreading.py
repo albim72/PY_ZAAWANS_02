301e65aedd372def8a7b3417a6195b286c57e1ec
albim72 <marcin.albiniak72@gmail.com>
2023-02-15 12:18:47 +0100

    Create multithreading.py

---
+++ DZIEN_3/multithreading.py
@@ -0,0 +1,20 @@
+import threading
+
+def worker(num,shared_variable):
+    shared_variable.append(num)
+    print(f"Wątek {threading.current_thread().name} dodał {num} do zmiennej współdzielonej")
+
+#zmienna współdzielona
+shared_variable = []
+
+threads = []
+
+for i in range(5):
+    t = threading.Thread(target=worker, args=(i, shared_variable))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+print(f"zmienna współdzielona po zakończeniu proacy wątków: {shared_variable}")
